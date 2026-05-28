c1cf50a50d742aeac4e40db481293793fa8ab971
Eltonjemusse <eltonjemussek7@gmail.com>
2026-05-28 19:42:18 +0100

    Corrige CORS para frontend online

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const adminRoutes = require('./routes/adminRoutes');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/admin', adminRoutes);
+
 app.get('/', (req, res) => {
   res.json({
     message: 'AGROMARKT API funcionando 🚜'
